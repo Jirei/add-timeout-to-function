41329be694f61522b6483dcd45ef5d4299240b93
Jirei <jirei.contact@gmail.com>
2023-12-24 07:31:20 +0100

    Correction

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     lib: {
       formats: ['es'],
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, './src/index.ts'),
       fileName:"index"
     }
   }
