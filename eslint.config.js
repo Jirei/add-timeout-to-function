2de69cbc8a543fe0e7cfa92932cfa41d1996ab54
Jirei <jirei.contact@gmail.com>
2024-04-20 00:03:04 +0200

    Added ESLint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  { ignores: ["dist", "coverage"] },
+);
