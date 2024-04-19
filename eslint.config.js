35af9aff42e74c5bb2e7ad77f6c7a55b97b5125f
Jirei <jirei.contact@gmail.com>
2024-04-20 01:50:26 +0200

    Added prettier plugin for eslint and corrected errors

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,12 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       parserOptions: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [
-    dts({include:"./src/index.ts"})
-  ],
+  plugins: [dts({ include: "./src/index.ts" })],
   build: {
     lib: {
-      formats: ['es','umd'],
-      entry: resolve(__dirname, './src/index.ts'),
-      fileName:"index",
-      name:"AddPromiseToFunction"
-    }
-  }
+      formats: ["es", "umd"],
+      entry: resolve(__dirname, "./src/index.ts"),
+      fileName: "index",
+      name: "AddPromiseToFunction",
+    },
+  },
 });
